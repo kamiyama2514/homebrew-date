9cdfd1784bfaa7aecada628692e22d98e3d62aef
kamiyama2514 <kmc2514@kamiyama.ac.jp>
2025-10-28 11:40:50 +0900

    version "1.0.0"

--- dote.rb
+++ dote.rb
@@ -3,6 +3,7 @@ class Dote < Formula
   homepage "https://github.com/kamiyama2514/homebrew-dote"
   url "https://raw.githubusercontent.com/kamiyama2514/homebrew-dote/main/dote.c"
   sha256 "5cb81675831b36745a9b7e94f779881603118a78067d8ea737bf915e870f96d9"
+  version "1.0.0"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
